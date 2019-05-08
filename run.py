fcef74e22ac308c2e97f5e0dc322ef1b7c532298
Stephane Menard <stephane@friendstrip.com>
2019-05-08 10:39:17 -0400

    run.py prints result

--- run.py
+++ run.py
@@ -5,7 +5,12 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')  # no prefix
 
 dim_ner = DimensionBertNer('models/dimension_ner_bert_best.pt')
 
-dim_ner.predict(['I would like to resize the previous image at 1024x768',
-                 'Resize to 800x600',
-                 'A height of 768 and a width of 1024 would be perfect',
-                ])
+dialog_sentences = ['I would like to resize the previous image at 1024x768',
+                    'Resize to 800 and 600',
+                    'A height of 768 and a width of 1024',
+                    ]
+
+dims = dim_ner.predict(dialog_sentences)
+
+for sentence, dim in zip(dialog_sentences, dims):
+    print(f"{sentence} \t {dim}")
